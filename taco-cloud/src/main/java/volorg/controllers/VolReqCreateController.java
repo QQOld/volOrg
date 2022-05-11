246fe582a0af75f700d84a05bcca3fa3723bec64
QQOld <artem.bataev2016@yandex.ru>
2022-05-11 22:27:51 +0300

    dsdsererer

--- taco-cloud/src/main/java/volorg/controllers/VolReqCreateController.java
+++ taco-cloud/src/main/java/volorg/controllers/VolReqCreateController.java
@@ -1,12 +1,18 @@
 package volorg.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import volorg.models.User;
 import volorg.models.VolRequest;
+import volorg.repositories.UserRepository;
 import volorg.repositories.VolRequestRepository;
 
 @Controller
@@ -14,11 +20,13 @@ import volorg.repositories.VolRequestRepository;
 public class VolReqCreateController {
 	
 private final VolRequestRepository volReqRepo;
+private final UserRepository userRepo;
 	
 	@Autowired
 	public VolReqCreateController(
-			VolRequestRepository volReqRepo) {
+			VolRequestRepository volReqRepo, UserRepository userRepo) {
 	  this.volReqRepo = volReqRepo;
+	  this.userRepo = userRepo;
 	}
 	
 	@ModelAttribute(name = "volReq")
@@ -27,8 +35,24 @@ private final VolRequestRepository volReqRepo;
 	}
 	
 	@GetMapping
-	public String volReq() {
+	public String volReqForm() {
 	  return "volReq";
+	}
+	
+	@PostMapping
+	public String addVolReq(@Valid VolRequest volReq, Errors errors) {
+	  if (errors.hasErrors()) {
+	    return "volReq";
+	  }
+	  User user = new User();
+	  user.setName("admin");
+	  user.setSurName("admin");
+	  userRepo.save(user);
+	  
+	  volReq.setStatus("Ожидание");
+	  volReq.setUser(user);
+	  volReqRepo.save(volReq);
+	  return "redirect:/index";
 	}
 
 }
